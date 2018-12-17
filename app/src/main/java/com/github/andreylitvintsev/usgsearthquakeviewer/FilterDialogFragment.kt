f1ec1c69c38a09ff24a28c96e33466a91b6b6e1b
Andrey Litvintsev <laa@wmr.ru>
2018-12-17 11:20:55 +0300

    Understand how working viewpager

--- app/src/main/java/com/github/andreylitvintsev/usgsearthquakeviewer/FilterDialogFragment.kt
+++ app/src/main/java/com/github/andreylitvintsev/usgsearthquakeviewer/FilterDialogFragment.kt
@@ -1,12 +1,15 @@
 package com.github.andreylitvintsev.usgsearthquakeviewer
 
 import android.app.Dialog
+import android.content.Context
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.view.PagerAdapter
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatDialogFragment
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,34 +21,64 @@ class FilterDialogFragment : AppCompatDialogFragment() { // TODO: посмотр
         return AlertDialog.Builder(appCompatActivity())
             .setView(appCompatActivity().layoutInflater.inflate(R.layout.dialog_fragment_filter, null).apply {
                 val viewPager = this.findViewById<ViewPager>(R.id.viewPager)
-                viewPager.adapter = object : FragmentPagerAdapter(appCompatActivity().supportFragmentManager) {
-                    override fun getItem(p0: Int): Fragment = when (p0) {
-//                        0 -> MagnitudeRangePickerFragment()
-                        else -> DatePickerFragment()
-                    }
-//
-                    override fun getCount(): Int = 2
-                }
+                viewPager.adapter = MyPagerAdapter(appCompatActivity().layoutInflater)
             })
             .create()
     }
 }
 
-class MagnitudeRangePickerFragment : Fragment() {
+class MyViewPager(context: Context, attribureSet: AttributeSet?) : ViewPager(context, attribureSet) {
 
-//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        return View(context).apply {
-//            setBackgroundColor(Color.RED)
-//            layoutParams =
-//                    ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
-//        }
-//    }
+    override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) { // TODO: отрефакторить
+        var heightMeasureSpec = heightMeasureSpec
+        val mode = View.MeasureSpec.getMode(heightMeasureSpec)
+        // Unspecified means that the ViewPager is in a ScrollView WRAP_CONTENT.
+        // At Most means that the ViewPager is not in a ScrollView WRAP_CONTENT.
+        if (mode == View.MeasureSpec.UNSPECIFIED || mode == View.MeasureSpec.AT_MOST) {
+            // super has to be called in the beginning so the child views can be initialized.
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+            var height = 0
+            for (i in 0 until childCount) {
+                val child = getChildAt(i)
+                child.measure(widthMeasureSpec, View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED))
+                val h = child.measuredHeight
+                if (h > height) height = h
+            }
+            heightMeasureSpec = View.MeasureSpec.makeMeasureSpec(height, View.MeasureSpec.EXACTLY)
+        }
+        // super has to be called again so the new specs are treated as exact measurements
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+    }
+
+}
+
+// TODO: разобраться с размером
+class MyPagerAdapter(private val layoutInflater: LayoutInflater) : PagerAdapter() {
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val view = layoutInflater.inflate(R.layout.reduce_layout, container, false)
+        container.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, view: Any) {
+        container.removeView(view as View)
+    }
+
+    override fun isViewFromObject(p0: View, p1: Any): Boolean {
+        return p0 == p1
+    }
+
+    override fun getCount(): Int {
+        return 2
+    }
+
 }
 
 class DatePickerFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.reduce_layout, container, false)
+        return inflater.inflate(R.layout.reduce_layout, null)
     }
 
 }
